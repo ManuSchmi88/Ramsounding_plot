4af91df59be6b63ab33c0b2d0f5708a39438f972
mschmid <Manuel.Schmid@uni-tuebingen.de>
2018-11-06 18:25:06 +0100

    create ramsound class

--- ramsound_plot.py
+++ ramsound_plot.py
@@ -29,5 +29,48 @@ def open_file(filename, filetype = 'ascii'):
 
 def convert_to_int(data):
     '''
-    
+    Convert data from datafile to ints. 
+    N10-values are always of type(int) so its ok.
     '''
+    data = [int(i) for i in data]
+
+    return data
+
+class ramsounding(self):
+    '''
+    Create class which holds the information about a individual ramsounding.
+    This can/needs to go somewhere else later.
+    '''
+
+    def __init__(self, rs_type,  zero_elevation = 0):
+        '''
+        input parameters: 
+
+        rs_type :   DPL - Light Ramsounding
+                    DPM - Medium Ramsounding
+                    DPH - Heavy Ramsounding
+
+        zero_elevation: Defines in which depth you started counting your N10 values
+        '''
+
+        self.rs_type = rs_type
+        self.zero_elevation = zero_elevation
+    
+    def attach_n10_values(self, filepath):
+        '''
+        Reads the functions to read and convert the original datafiles
+        and attaches it to self
+        '''
+
+        self.n10_values = convert_to_int(open_file(filepath))
+
+    def calculate_total_depth(self):
+        '''
+        calculates the total depth
+        Right now this assumes that we are talking about N10 values
+        so therefore they were counted in 10cm intervals.
+        '''
+
+        self.total_depth = self.zero_elevation + len(self.n10_values) * 0.1 
+
+    
