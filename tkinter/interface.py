2ab6b9a72cd6784303090c2c734845fa3e21f33a
mschmid <Manuel.Schmid@uni-tuebingen.de>
2019-03-08 09:32:39 +0100

    new

--- tkinter/interface.py
+++ tkinter/interface.py
@@ -6,7 +6,6 @@ Created by Manuel Schmid, Arbeitsloser Tunichtgut, 7.Februar 2019
 
 import tkinter as tk
 from tkinter.filedialog import askopenfilename
-from tkinter.filedialog import asksaveasfilename
 from tkinter.filedialog import askdirectory
 import matplotlib
 matplotlib.use('TkAgg')
@@ -26,10 +25,9 @@ class Data_provider(object):
     Class which is used to hold data for plotting
     """
     def __init__(self):
-
+    
         self.data_container = {}
         self.depth_array = 0
-        
         self.nn_data_array = 0
         self.nn_depth_array = 0
 
@@ -41,10 +39,7 @@ class Data_provider(object):
         
         #populate data_container with values
         for d in data_string_array:
-            self.data_container[d] = 0
-
-        self.data_container['interval'] = 0.1
-        self.data_container['zero_elevation'] = 0
+            self.data_container[d] = []
 
 class Calculator(object):
 
@@ -52,9 +47,9 @@ class Calculator(object):
         """
         calculate total depth from raw_data
         """
-        rms_depth = container.data_container['zero_elevation'] + float(int(len(container.data_container['raw_data'])) * 0.1) #TODO: contains hardcoded data
-        
-        return float(rms_depth)
+        rms_depth = 0 + len(container.data_container['raw_data'] * 0.1) #TODO: contains hardcoded data
+
+        return rms_depth
 
     def convert_data_to_plot_format(self, container):
         """
@@ -63,8 +58,8 @@ class Calculator(object):
         """
 
         #expand depth_array to get a more nearest-neighbour like look
-        nn_depth_array = np.arange(0, container.data_container['rms_depth'], container.data_container['interval'] / 100)
-        nn_data_array = np.repeat(container.data_container['raw_data'], 100)
+        nn_depth_array = np.arange(0, container.data_container['rms_depth'], 0.1 / 100)
+        nn_data_array = np.repeat(container.data_container['raw_data'], 1000)
 
         return nn_depth_array, nn_data_array
         
@@ -89,7 +84,7 @@ class App(tk.Tk):
         #dictionarty which holds all the pages
         self.frames = {}
 
-        for F in (landing_page, figure_page):
+        for F in (landing_page, figure_page, multiple_figures_page):
             frame = F(master_frame, self)
             self.frames[F] = frame
             frame.grid(row = 0, column = 0, sticky = 'nsew')
@@ -107,17 +102,18 @@ class landing_page(tk.Frame):
     First Page that pops up if programm is started. 
     """
     def __init__(self, parent, controller):
-                
+        
         self.controller = controller
 
         tk.Frame.__init__(self, parent)
 
         self.front_label = tk.Label(self, text = "Rammsondier Plotter", font = ("Arial Bold", 16))
         #self.front_label.pack()
-        self.front_label.grid(column = 0, row = 0, padx = 50)
+        self.front_label.grid(column = 0, row = 0, padx = 50, pady = 15)
         self.button_open_file = tk.Button(
-            self, text = 'Öffne .dat Dateien', fg = 'red', command = self.open_file)
-        self.button_open_file.grid(column = 0, row = 1)
+            self, text = 'Öffne .dat Dateien', fg = 'red', command = self.open_file
+            )
+        self.button_open_file.grid(column = 0, row = 1, pady = 15)
 
         self.button_load_directory = tk.Button(self, text = 'Öffne Ordner', command=self.open_folder)
         self.button_load_directory.grid(column = 0, row = 2)
@@ -129,7 +125,7 @@ class landing_page(tk.Frame):
         filename = askopenfilename()
         dp.data_container['filename'] = filename
         print('Accessing file: ' + dp.data_container['filename'])
-        self.load_data(filename)
+        self.load_data()
         self.controller.show_frame(figure_page)
               
     def open_folder(self):
@@ -139,12 +135,11 @@ class landing_page(tk.Frame):
         foldername = askdirectory()
         self.foldername = foldername
 
-    def load_data(self, filename):
+    def load_data(self):
         """
         loads .dat file into data_container and does some calculations
         """
-        #file = open(dp.data_container['filename'], 'r')
-        file = open(filename, 'r')
+        file = open(dp.data_container['filename'], 'r')
         data = file.readlines()
         file.close()
         data_int = [int(i) for i in data]
@@ -152,21 +147,13 @@ class landing_page(tk.Frame):
 
         dp.data_container['raw_data'] = data_int
         dp.data_container['rms_depth'] = ca.get_total_depth(dp)
-        dp.nn_depth_array , dp.nn_data_array = ca.convert_data_to_plot_format(dp)
+        dp.nn_data_array , dp.nn_depth_array = ca.convert_data_to_plot_format(dp)
 
-        global test1
-        global test2
-        test1 = dp.nn_data_array
-        test2 = dp.nn_depth_array
-
-        print(test1, test2)
-        
         #DEBUG
-        #print(dp.nn_data_array)
-        #print(dp.data_container['rms_depth'])
+        print(dp.nn_data_array)
+        print(dp.data_container['rms_depth'])
 
 class figure_page(tk.Frame):
-
     def __init__(self, parent, controller):
   
         #add the controller to self.namespace
@@ -179,119 +166,58 @@ class figure_page(tk.Frame):
 
         #button to navigate back to main_frame
         self.back_button = tk.Button(self, text = 'Back', command = self.back_to_home)
-        self.back_button.grid(row = 0, column = 4) 
+        self.back_button.grid(row = 0, column = 1) 
 
         self.print_button = tk.Button(self, text = 'Show', command = self.show_figure)
         self.print_button.grid(row = 0, column = 2)
 
-        self.print_button = tk.Button(self, text = 'Save', command = self.save_figure)
-        self.print_button.grid(row = 0, column = 3)
-
-        self.color_bool = tk.IntVar()
-        self.color_check = tk.Checkbutton(self, text="Colorcoding", variable=self.color_bool)
-        self.color_check.grid(row = 4, column = 4)
-
-        self.fill_between_bool = tk.IntVar()
-        self.fill_check = tk.Checkbutton(self, text = "Fill between", variable = self.fill_between_bool)
-        self.fill_check.grid(row = 4, column = 2)
-
-        self.grid_bool = tk.IntVar()
-        self.grid_check = tk.Checkbutton(self, text = "Grid", variable = self.grid_bool)
-        self.grid_check.grid(row = 4, column = 3)
-
-        self.show_figure()
+        f = Figure()
 
     def show_figure(self):
+        print('Show figure function was called!')
 
-        print('show_figure function was called!')
+        print(dp.nn_data_array)
 
-        self.fig, self.ax = plt.subplots(1)
-        self.ax.plot(dp.nn_data_array, dp.nn_depth_array, 'k', linewidth = 2.3)
-        self.ax.set_xlim([0,10])
-        lower_ylim = round(dp.data_container['rms_depth'] + 0.5)
-        self.ax.set_ylim(lower_ylim,0)
-        self.ax.xaxis.tick_top()
-        canvas = FigureCanvasTkAgg(self.fig, self)
+        fig, ax = plt.subplots(1)
+        ax.plot(dp.nn_data_array, dp.nn_depth_array)
+        canvas = FigureCanvasTkAgg(fig, self)
         canvas.draw()
+
         canvas.get_tk_widget().grid(column = 1, row = 1, pady = 15)
-        #toolbar = NavigationToolbar2Tk(canvas, self)
-        #toolbar.update()
-        if self.color_bool.get() == 1:
-            self.colorcode_indices(self.fig, self.ax)
-
-        if self.fill_between_bool.get() == 1:
-            self.fill_between(self.fig, self.ax)
-
-        if self.grid_bool.get() == 1:
-            self.with_grid(self.fig, self.ax)
-
-    def save_figure(self):
-
-        print('save_figure function was called!')
-
-        filename = asksaveasfilename()
-        plt.savefig(str(filename))
-
-    def colorcode_indices(self, fig, ax):
-        """
-        If this function is called the figure will be colorcoded 
-        according to the different "steifigkeits-indizes"
-
-        weich : 0 - 4
-        breiig: 4 - 8
-        steif:  8 - 14
-        halbfest: 14 - 28
-        fest: > 28
-        """
-
-        print('colorcode function was called')
-
-        _data = dp.nn_data_array
-        weich_indices    = np.where(np.logical_and(_data >= 0, _data < 4))
-        breiig_indices   = np.where(np.logical_and(_data >= 4, _data < 8))
-        steif_indices    = np.where(np.logical_and(_data >= 8, _data < 14))
-        halbfest_indices = np.where(np.logical_and(_data >= 14, _data < 28))
-        fest_indices     = np.where(_data >= 28)
-
-
-        for i in weich_indices:
-            ax.hlines(dp.nn_depth_array[i],dp.nn_data_array[i] ,90, alpha = 0.01)
-        for i in breiig_indices:
-            ax.hlines(dp.nn_depth_array[i],dp.nn_data_array[i] ,90, alpha = 0.01, color = 'blue')
-        for i in steif_indices:
-            ax.hlines(dp.nn_depth_array[i],dp.nn_data_array[i] ,90, alpha = 0.01, color = 'magenta')
-        for i in halbfest_indices:
-            ax.hlines(dp.nn_depth_array[i],dp.nn_data_array[i] ,90, alpha = 0.01, color = 'yellow')
-        for i in fest_indices:
-            ax.hlines(dp.nn_depth_array[i],dp.nn_data_array[i] ,90, alpha = 0.02, color = 'red')
-
-
-    def fill_between(self, fig, ax):
-        """
-        If box is checked, it fills the space between the y-axis and the values
-        """
-
-        ax.fill_betweenx(dp.nn_depth_array, dp.nn_data_array, hatch = '/')
-
-    def with_grid(self, fig, ax):
-        """
-        If box is checked, plot gets a grid
-        """
-        ax.grid(linewidth = 1.2)
 
     def back_to_home(self):
         """
         This probably can get a global function. 
         Navigates back to home
         """
-        #plt.close(self.fig)
         self.controller.show_frame(landing_page)
 
+class multiple_figures_page(tk.Frame):
+    def __init__(self, parent, controller):
+
+        #add the controller to self.namespace
+        self.controller = controller
+
+        tk.Frame.__init__(self,parent)
+        self.multiple_figures_label = tk.Label(self, text = 'Multiple Figures', font = large_font)
+        self.multiple_figures_label.grid(column = 0, row = 0)
+
+        #button to navigate back to main_frame
+        self.back_button = tk.Button(self, text = 'Back', command = self.back_to_home)
+        self.back_button.grid(row = 0, column = 1)
+        
+    def back_to_home(self):
+        """
+        This probably can get a global function. 
+        Navigates back to home
+        """
+        self.controller.show_frame(landing_page)
+
+
 #initialise data_container class globally
-if __name__ == '__main__':
-    dp = Data_provider()
-    ca = Calculator()
-    app = App()
+dp = Data_provider()
+ca = Calculator()
+app = App()
 
 app.mainloop()
 #root.destroy()
